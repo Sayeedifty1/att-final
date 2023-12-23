ac645a219b8ab9c4c78e9d5efb35fad5f17a0536
Sayeedifty1 <md.abusayeedifty@gmail.com>
2023-12-23 12:11:51 +0600

    updated

--- index.js
+++ index.js
@@ -1,21 +1,17 @@
 const express = require('express');
 const cors = require('cors');
-const app = express();
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config();
 
+const app = express();
 const port = process.env.PORT || 3000;
 
-// ! Middleware
+// Middleware
 app.use(cors());
 app.use(express.json());
 
-
-
-// ! MongoDB Connection
-const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+// MongoDB Connection
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.nsyuaxc.mongodb.net/?retryWrites=true&w=majority`;
-
-// Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
     serverApi: {
         version: ServerApiVersion.v1,
@@ -27,15 +23,10 @@ const client = new MongoClient(uri, {
     maxPoolSize: 50,
 });
 
-async function run() {
+async function connectToMongoDB() {
     try {
-        // Connect the client to the server	(optional starting in v4.7)
-        await client.connect((err => {
-            if (err) {
-                console.log(err);
-                return;
-            }
-        }));
+        await client.connect();
+        console.log("Connected to MongoDB!")
         const usersCollection = client.db("premier1").collection("Users");
         const newPrintCollection = client.db("premier1").collection("newReg");
         const attCollection = client.db("premier1").collection("attendance");
@@ -361,15 +352,17 @@ async function run() {
 
 
 
-        // Send a ping to confirm a successful connection
         await client.db("admin").command({ ping: 1 });
         console.log("Pinged your deployment. You successfully connected to MongoDB!");
-    } finally {
-        // Ensures that the client will close when you finish/error
-        // await client.close();
+
+    } catch (error) {
+        console.error("Error connecting to MongoDB:", error);
+        // You might want to handle the error appropriately.
     }
 }
-run().catch(console.dir);
+
+// Call the function to connect to MongoDB
+connectToMongoDB();
 
 
 app.get('/', (req, res) => {
